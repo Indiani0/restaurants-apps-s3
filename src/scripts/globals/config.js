f73cb93e17bb039911c5e01ad304b8f8888899ee
Indiani0 <srirahayu200207@gmail.com>
2022-11-13 09:10:08 +0700

    Initialize restaurants-apps automated testing implemented

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_URL: (size) => `https://restaurant-api.dicoding.dev/images/${size}/`,
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'GrammRestaurant-V1',
+
+  DATABASE_NAME: 'gramm-restaurant-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants'
+
+};
+
+export default CONFIG;
